35a8df7773ee53eb3357bb89a0a3414fe96da85a
DESKTOP-NU7UV56\tetep <tetepiti149@naver.com>
2022-09-14 09:03:27 +0900

    메인 페이지 추가

---
+++ vue3practice/src/components/NavBar.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="navBar">
+    <div class="navBar-logo"></div>
+    <div style="display: flex">
+      <div class="navBar-btn">마이페이지</div>
+      <div class="navBar-btn">로그아웃</div>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style>
+.navBar {
+  width: 100%;
+  height: 100px;
+  display: flex;
+  justify-content: space-between;
+  top: 0;
+}
+.navBar-logo {
+  width: 100px;
+  height: 100px;
+  background-color: aqua;
+}
+.navBar-btn {
+  width: 200px;
+  height: 100px;
+  display: flex;
+  font-size: 30px;
+  justify-content: center;
+  align-items: center;
+}
+</style>
