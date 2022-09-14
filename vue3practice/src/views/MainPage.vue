72096d195479dea47f72a8629b768c3feb112279
DESKTOP-NU7UV56\tetep <tetepiti149@naver.com>
2022-09-14 10:18:44 +0900

    화면 높이, 너비 가져오기 이벤트 리스너 추가

--- vue3practice/src/views/MainPage.vue
+++ vue3practice/src/views/MainPage.vue
@@ -1,7 +1,46 @@
 <template>
-  <div>메인페이지</div>
+  <div class="mainpage">메인페이지
+    <div style="margin-top: 340px; height:60px;">
+      <a>지친 당신을 위한 여행지 추천 서비스</a>
+    </div>
+    <div style="height:220px;">
+      여기다
+    </div>
+    <div>
+      <p>{{height}}</p>
+      <p>{{width}}</p>
+    </div>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { onBeforeUnmount, onMounted, ref } from 'vue';
+const height = ref(null);
+const width = ref(null);
 
-<style></style>
+function resize() {
+  height.value = window.innerHeight;
+  width.value = window.innerWidth;
+}
+
+onMounted(() => {
+  resize()
+  window.addEventListener("resize", resize);
+});
+
+onBeforeUnmount(() => {
+  window.removeEventListener("resize", resize);
+});
+</script>
+
+<style>
+.mainpage {
+  min-height: 100%;
+  width: 100%;
+  background-image: url("../assets/main1-1.jpg");
+  background-size: cover;
+  position: fixed;
+  margin-top: -100px;
+  z-index: 0;
+}
+</style>
